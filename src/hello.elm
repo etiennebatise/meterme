140dd03d85bc9dd15e138602cd3ca3fe74f3d57a
Etienne Batise <etienne.batise@gmail.com>
2019-01-31 23:14:45 +0100

    Add multiple counters with complex record types

--- src/hello.elm
+++ src/hello.elm
@@ -5,24 +5,56 @@ import Html exposing (Html, div, button, text)
 import Html.Events exposing (onClick)
 
 
-type Msg = Increment | Decrement
+type Msg = IncrementSquat | DecrementSquat
+         | IncrementPullUp | DecrementPullUp
+         | IncrementBenchPress | DecrementBenchPress
+         | IncrementOverheadPress | DecrementOverheadPress
 
-type alias Model = Int
+type alias Model = {
+        squat : Int
+      , pullUp : Int
+      , benchPress : Int
+      , overheadPress : Int
+    }
 
--- update :: Msg -> Model -> _
 update msg model =
     case msg of
-        Increment ->
-            model + 1
-        Decrement ->
-            model - 1
+        IncrementSquat ->
+            {model | squat = model.squat + 1}
+        DecrementSquat ->
+            {model | squat = model.squat - 1}
+        IncrementPullUp ->
+            {model | pullUp = model.pullUp  + 1}
+        DecrementPullUp ->
+            {model | pullUp = model.pullUp  - 1}
+        IncrementBenchPress ->
+            {model | benchPress = model.benchPress  + 1}
+        DecrementBenchPress ->
+            {model | benchPress = model.benchPress  - 1}
+        IncrementOverheadPress ->
+            {model | overheadPress = model.overheadPress  + 1}
+        DecrementOverheadPress ->
+            {model | overheadPress = model.overheadPress  - 1}
+
+opTemplate opLabel msg = button [onClick msg] [text opLabel]
+
+incTemplate = opTemplate "+"
+
+decTemplate = opTemplate "-"
+
+viewTemplate name value msgA msgB = div []
+                                    [ div [] [text (String.fromInt value)]
+                                    , decTemplate msgA
+                                    , incTemplate msgB
+                                    ]
 
 view model = div []
-             [ div [] [text (String.fromInt  model)]
-             , button [onClick Decrement] [text "-"]
-             , button [onClick Increment] [text "+"]
+             [ viewTemplate "Squats" model.squat DecrementSquat IncrementSquat
+             , viewTemplate "PullUps" model.pullUp DecrementPullUp IncrementPullUp
+             , viewTemplate "BenchPress" model.benchPress DecrementBenchPress IncrementBenchPress
+             , viewTemplate "OverheadPresss" model.overheadPress DecrementOverheadPress IncrementOverheadPress
              ]
 
-init = 0
+init = { squat = 0, pullUp = 0, benchPress = 0, overheadPress = 0 }
 
 main = Browser.sandbox { init=init, update=update, view=view }
