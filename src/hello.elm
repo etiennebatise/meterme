967f428a0dd2ead133e9ba6e276e0db4ccd450b9
Etienne Batise <etienne.batise@gmail.com>
2019-01-31 22:41:24 +0100

    Add basic counter

--- src/hello.elm
+++ src/hello.elm
@@ -1,5 +1,28 @@
 module HelloWorld exposing (..)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, div, button, text)
+import Html.Events exposing (onClick)
 
-main = text "Hello, World!"
+
+type Msg = Increment | Decrement
+
+type alias Model = Int
+
+-- update :: Msg -> Model -> _
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+        Decrement ->
+            model - 1
+
+view model = div []
+             [ div [] [text (String.fromInt  model)]
+             , button [onClick Decrement] [text "-"]
+             , button [onClick Increment] [text "+"]
+             ]
+
+init = 0
+
+main = Browser.sandbox { init=init, update=update, view=view }
