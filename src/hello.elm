d7649ef55c98cc1f28374eab5595eb14dcbd353f
Etienne Batise <etienne.batise@gmail.com>
2019-02-12 22:41:52 +0100

    Enable selecting a number of reps for 1 serie

--- src/hello.elm
+++ src/hello.elm
@@ -1,15 +1,16 @@
 module HelloWorld exposing (Model, Msg(..), Series, init, main, update, view)
 
 import Browser
-import Html exposing (Attribute, Html, button, div, h1, input, option, select, text, br)
+import Html exposing (Attribute, Html, br, button, div, h1, input, option, select, text)
 import Html.Attributes exposing (selected, value)
 import Html.Events exposing (onClick, onInput)
-import List exposing (append, concat, indexedMap, length, map, range, intersperse)
+import List exposing (append, concat, indexedMap, intersperse, length, map, range)
 import Maybe
 
 
 type Msg
     = UpdateExercice Int String
+    | UpdateSerie Int String
     | CreateSession
     | AddExercise
 
@@ -50,7 +51,8 @@ emptyExercise name =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        AddExercise -> append model [ emptyExercise "squat" ]
+        AddExercise ->
+            append model [ emptyExercise "squat" ]
 
         UpdateExercice index name ->
             indexedMap
@@ -63,18 +65,37 @@ update msg model =
                 )
                 model
 
+        UpdateSerie index nbReps ->
+            indexedMap
+                (\i e ->
+                    if i == index then
+                        { e | training = [ Series (Maybe.withDefault 0 <| String.toInt nbReps) 0]}
+
+                    else
+                        e
+                )
+                model
+
+
         CreateSession ->
             [ Exercise "squat" [] ]
 
 
-viewExercise : List (Attribute msg) -> Html msg
-viewExercise attrs =
-    select attrs
-        [ option [ value "squat" ] [ text "Squat" ]
-        , option [ value "pullup" ] [ text "Pull-Up" ]
-        , option [ value "benchpress" ] [ text "Bench-Press" ]
-        , option [ value "overheadpress" ] [ text "Overhead-Press" ]
-        ]
+optionRange : Int -> Int -> List (Html msg)
+optionRange from to = let l = range from to
+                      in map (\i -> option [value <| String.fromInt i] [text <| String.fromInt i]) l
+
+viewExercise : Int -> List (Attribute Msg) -> Html Msg
+viewExercise index attrs =
+    div []
+    [ select attrs
+          [ option [ value "squat" ] [ text "Squat" ]
+          , option [ value "pullup" ] [ text "Pull-Up" ]
+          , option [ value "benchpress" ] [ text "Bench-Press" ]
+          , option [ value "overheadpress" ] [ text "Overhead-Press" ]
+          ]
+    , select [onInput (UpdateSerie index)] (optionRange 1 12)
+    ]
 
 
 view : Model -> Html Msg
@@ -86,24 +107,12 @@ view model =
         l ->
             let
                 created =
-                    indexedMap (\i e -> viewExercise [ value e.name, onInput (UpdateExercice i) ]) l
+                    indexedMap (\i e -> viewExercise i [ value e.name, onInput (UpdateExercice i) ]) l
 
                 new =
-                     button [onClick AddExercise] [ text "Add exercice"]
+                    button [ onClick AddExercise ] [ text "Add exercice" ]
             in
             div [] <| intersperse (br [] []) <| append created [ new ]
-
-
-
--- The hole selectWeight, Reps... is wrong. You need to somehow pass in the model
--- This example shows it a bit
--- [ input
---   [ value newComment.title
---   , onInput (\v -> toUpdateComment { newComment | title = v })
---   ]
---   []
--- newComment is the model.
--- I think the right way here is to delete all of the view logic and rewrite it with the "onclick" thing in mind
 
 
 init : Model
