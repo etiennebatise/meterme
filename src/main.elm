232e07ba181850f37a9bf3ce69febaa0c30020fc
Etienne <etienne.batise@gmail.com>
2019-04-02 21:59:53 +0200

    Add text inputs to fill exercise data

--- src/main.elm
+++ src/main.elm
@@ -1,26 +1,25 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Attribute, Html, br, button, div, h1, input, option, select, text, form, label)
-import Html.Attributes exposing (selected, type_, value)
+import Html exposing (Attribute, Html, br, button, div, form, h1, input, label, option, select, text)
+import Html.Attributes exposing (selected, type_, value, name)
 import Html.Events exposing (onClick, onInput, onSubmit)
-import List exposing (append, concat, indexedMap, intersperse, length, map, range, take, head, drop, reverse)
-import Set exposing (fromList, toList, diff)
-import Maybe
-import List.Extra exposing (updateAt, indexedFoldr)
-import Maybe.Extra exposing (unwrap)
+import List exposing (append, concat, drop, head, indexedMap, intersperse, length, map, range, repeat, reverse, singleton, take)
+import List.Extra as ListE
+import Maybe as Maybe
+import Maybe.Extra as MaybeE
+import Set exposing (diff, fromList, toList)
+import String exposing (toInt, fromInt)
 
 
 type alias Series =
-    { numberOfReps : Int
+    { reps : Int
     , weight : Int
     }
 
-
-
 type alias Exercise =
     { name : String
-    , training : List Series
+    , series : List Series
     }
 
 
@@ -30,6 +29,8 @@ type Msg
     | AddExercise
     | ScheduleSession String
     | UpdateExerciseName Int String
+    | UpdateExerciseSeriesReps Int Int String
+    | UpdateExerciseSeriesWeight Int Int String
 
 
 type alias Model =
@@ -44,23 +45,39 @@ update msg model =
         NoOp ->
             model
 
-        SubmitForm
-            -> model
+        SubmitForm ->
+            model
 
         ScheduleSession date ->
-                    { model | date = date }
+            { model | date = date }
 
         AddExercise ->
-                    let
-                        selected = map (.name) model.session
-                        newExercise = unwrap [] (\n -> [Exercise n []]) <| head <| remainingExercises selected
-                    in
-                        { model | session = append model.session newExercise }
+            let
+                selected =
+                    map .name model.session
+
+                newExercise =
+                    MaybeE.unwrap [] (singleton << exercise) <| head <| remainingExercises selected
+            in
+            { model | session = append model.session newExercise }
 
         UpdateExerciseName index name ->
-                    { model | session = updateAt index (\e -> {e | name = name }) model.session}
+            { model | session = ListE.updateAt index (\e -> { e | name = name }) model.session }
+
+        UpdateExerciseSeriesReps ei si v ->
+            { model | session = ListE.updateAt ei (\e -> { e | series = ListE.updateAt si (\s -> {s | reps = Maybe.withDefault 0 <| toInt v}) e.series }) model.session }
+
+        UpdateExerciseSeriesWeight ei si v ->
+            { model | session = ListE.updateAt ei (\e -> { e | series = ListE.updateAt si (\s -> {s | weight = Maybe.withDefault 0 <| toInt v}) e.series }) model.session }
+
+exercise : String -> Exercise
+exercise n =
+    Exercise n <| repeat 4 series
 
 
+series : Series
+series =
+    Series 0 0
 
 
 datePicker : String -> Html Msg
@@ -72,45 +89,75 @@ datePicker date =
         ]
         []
 
+
 exerciseList : List String
-exerciseList = ["squat", "pull_up", "bench_press", "overhead_press"]
+exerciseList =
+    [ "squat", "pull_up", "bench_press", "overhead_press" ]
 
 
 remainingExercises : List String -> List String
-remainingExercises l = toList <| diff (fromList exerciseList) (fromList l)
+remainingExercises l =
+    toList <| diff (fromList exerciseList) (fromList l)
 
-selectExercise : Int -> Exercise -> List String -> Html Msg
-selectExercise i e l=
-    select
-        [ onInput <| UpdateExerciseName i
-        , value e.name ]
-        (map (\x -> option [value x, selected (x == e.name)] [text x]) l)
+
+viewSeries : Int -> Int -> Series -> Html Msg
+viewSeries ei si s =
+    div []
+        [ input [ type_ "number", name "reps", value <| fromInt s.reps, onInput <| UpdateExerciseSeriesReps ei si] []
+        , input [ type_ "number", name "weight", value <| fromInt s.weight, onInput <| UpdateExerciseSeriesWeight ei si] []
+        ]
+
+viewExercise : Int -> Exercise -> Html Msg
+viewExercise i e =
+    div
+        []
+        <| append
+            [ select
+                  [ onInput <| UpdateExerciseName i
+                  , value e.name
+                  ]
+                  (map (\x -> option [ value x, selected (x == e.name) ] [ text x ]) exerciseList)
+            ]
+            (indexedMap (viewSeries i) e.series)
 
 
 addExerciseButton : Html Msg
-addExerciseButton = button [ onClick AddExercise ] [ text "+"]
+addExerciseButton =
+    button [ onClick AddExercise ] [ text "+" ]
 
-selectExs : List Exercise -> List (Html Msg)
-selectExs exs =
+
+viewExercises : List Exercise -> List (Html Msg)
+viewExercises exs =
     let
-         names = map (.name) exs
-         remaining i = remainingExercises <| take i names
-         go i e l = append l [selectExercise i e (remaining i)]
-    in reverse <| indexedFoldr go [] exs
+        names =
+            map .name exs
+
+        remaining i =
+            remainingExercises <| take i names
+    in
+    indexedMap viewExercise exs
 
 
 view : Model -> Html Msg
 view model =
-    let dateLabel = label [] [ datePicker model.date ]
-        exs = selectExs model.session
-        content = intersperse (br [] []) <| append [ dateLabel ] <| append exs [ addExerciseButton ]
+    let
+        dateLabel =
+            label [] [ datePicker model.date ]
+
+        exercises =
+            viewExercises model.session
+
+        content =
+            intersperse (br [] []) <| append [ dateLabel ] <| append exercises [ addExerciseButton ]
     in
-      form
-          [ onSubmit SubmitForm ]
-          content
+    form
+        [ onSubmit SubmitForm ]
+        content
+
 
 init : Model
-init = { date = "", session = [] }
+init =
+    { date = "", session = [] }
 
 
 main =
