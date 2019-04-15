45fd00f22c1079e5480e85443b96b0e74262da82
Etienne <etienne.batise@gmail.com>
2019-04-15 22:27:39 +0200

    Format source

--- src/main.elm
+++ src/main.elm
@@ -1,9 +1,9 @@
 module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
+import Debug
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Attributes as Attr
+import Html.Attributes as Attr exposing (..)
 import Html.Events exposing (..)
 import Http
 import Json.Encode as Encode
@@ -16,8 +16,6 @@ import Platform.Cmd as Cmd
 import Platform.Sub as Sub
 import Set exposing (..)
 import String as Str
-
-import Debug
 
 
 type alias Series =
@@ -49,11 +47,11 @@ type alias Model =
     }
 
 
-update : Msg -> Model -> (Model, Cmd.Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd.Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
 
         SubmitForm ->
             ( model
@@ -66,9 +64,10 @@ update msg model =
 
         ScheduleSession date ->
             let
-                m = { model | date = date }
+                m =
+                    { model | date = date }
             in
-                (m, Cmd.none)
+            ( m, Cmd.none )
 
         AddExercise ->
             let
@@ -77,45 +76,66 @@ update msg model =
 
                 newExercise =
                     MaybeE.unwrap [] (List.singleton << exercise) <| head <| remainingExercises selected
-                m = { model | session = append model.session newExercise }
+
+                m =
+                    { model | session = append model.session newExercise }
             in
-                (m, Cmd.none)
+            ( m, Cmd.none )
 
         UpdateExerciseName index name ->
             let
-                m = { model | session = ListE.updateAt index (\e -> { e | name = name }) model.session }
+                m =
+                    { model | session = ListE.updateAt index (\e -> { e | name = name }) model.session }
             in
-                (m, Cmd.none)
+            ( m, Cmd.none )
 
         UpdateExerciseSeriesReps ei si v ->
             let
-                m = { model | session = ListE.updateAt ei (\e -> { e | series = ListE.updateAt si (\s -> { s | reps = Maybe.withDefault 0 <| Str.toInt v }) e.series }) model.session }
+                m =
+                    { model | session = ListE.updateAt ei (\e -> { e | series = ListE.updateAt si (\s -> { s | reps = Maybe.withDefault 0 <| Str.toInt v }) e.series }) model.session }
             in
-                (m, Cmd.none)
+            ( m, Cmd.none )
 
         UpdateExerciseSeriesWeight ei si v ->
             let
-                m = { model | session = ListE.updateAt ei (\e -> { e | series = ListE.updateAt si (\s -> { s | weight = Maybe.withDefault 0 <| Str.toInt v }) e.series }) model.session }
+                m =
+                    { model | session = ListE.updateAt ei (\e -> { e | series = ListE.updateAt si (\s -> { s | weight = Maybe.withDefault 0 <| Str.toInt v }) e.series }) model.session }
             in
-                (m, Cmd.none)
-        Uploaded _ -> ({model | session = []}, let a = Debug.log "debug" model in Cmd.none)
+            ( m, Cmd.none )
+
+        Uploaded _ ->
+            ( { model | session = [] }
+            , let
+                a =
+                    Debug.log "debug" model
+              in
+              Cmd.none
+            )
+
 
 encodeModel : Model -> Encode.Value
-encodeModel m = Encode.object
-                [ ("date", Encode.string m.date)
-                , ("session", Encode.list encodeExercise m.session)
-                ]
+encodeModel m =
+    Encode.object
+        [ ( "date", Encode.string m.date )
+        , ( "session", Encode.list encodeExercise m.session )
+        ]
+
 
 encodeExercise : Exercise -> Encode.Value
-encodeExercise e = Encode.object
-                   [ ("name", Encode.string e.name)
-                   , ("series", Encode.list encodeSeries e.series)
-                   ]
+encodeExercise e =
+    Encode.object
+        [ ( "name", Encode.string e.name )
+        , ( "series", Encode.list encodeSeries e.series )
+        ]
+
+
 encodeSeries : Series -> Encode.Value
-encodeSeries s = Encode.object
-                 [ ("weight", Encode.int s.weight)
-                 , ("reps", Encode.int s.reps)
-                 ]
+encodeSeries s =
+    Encode.object
+        [ ( "weight", Encode.int s.weight )
+        , ( "reps", Encode.int s.reps )
+        ]
+
 
 exercise : String -> Exercise
 exercise n =
@@ -146,29 +166,36 @@ remainingExercises : List String -> List String
 remainingExercises l =
     toList <| diff (fromList exerciseList) (fromList l)
 
+
 viewSeries : Int -> Int -> Series -> Html Msg
 viewSeries ei si s =
     let
-        minReps = 0
-        maxReps = 12
+        minReps =
+            0
+
+        maxReps =
+            12
     in
-        div []
-            [  input
-                  [ type_ "number"
-                  , name "weight"
-                  , Attr.min "0"
-                  , value <| Str.fromInt s.weight
-                  , onInput <| UpdateExerciseSeriesWeight ei si ] []
-            ,  input
-                  [ type_ "range"
-                  , name "reps"
-                  , Attr.max <| Str.fromInt maxReps
-                  , Attr.min <| Str.fromInt minReps
-                  , value <| Str.fromInt s.reps
-                  , onInput <| UpdateExerciseSeriesReps ei si ]
-                  []
-            , text <| Str.fromInt s.reps
+    div []
+        [ input
+            [ type_ "number"
+            , name "weight"
+            , Attr.min "0"
+            , value <| Str.fromInt s.weight
+            , onInput <| UpdateExerciseSeriesWeight ei si
             ]
+            []
+        , input
+            [ type_ "range"
+            , name "reps"
+            , Attr.max <| Str.fromInt maxReps
+            , Attr.min <| Str.fromInt minReps
+            , value <| Str.fromInt s.reps
+            , onInput <| UpdateExerciseSeriesReps ei si
+            ]
+            []
+        , text <| Str.fromInt s.reps
+        ]
 
 
 viewExercise : Int -> Exercise -> Html Msg
@@ -189,6 +216,7 @@ viewExercise i e =
 addExerciseButton : Html Msg
 addExerciseButton =
     button [ onClick AddExercise ] [ text "+" ]
+
 
 addSendButton : Html Msg
 addSendButton =
@@ -217,21 +245,25 @@ view model =
             viewExercises model.session
 
         modifierButton =
-            if length model.session == 4
-              then addSendButton
-              else addExerciseButton
+            if length model.session == 4 then
+                addSendButton
+
+            else
+                addExerciseButton
 
         formContent =
             [ div [] [ dateLabel ]
             , div [] exercises
             ]
 
-        content = div
-                  []
-                  [ Html.form [ id "workout-form" ] formContent
-                  , div [] [ modifierButton ]
-                  ]
-    in content
+        content =
+            div
+                []
+                [ Html.form [ id "workout-form" ] formContent
+                , div [] [ modifierButton ]
+                ]
+    in
+    content
 
 
 init : Model
@@ -242,7 +274,8 @@ init =
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \flags -> (init, Cmd.none)
+        { init = \flags -> ( init, Cmd.none )
         , subscriptions = \_ -> Sub.none
         , update = update
-        , view = view }
+        , view = view
+        }
